ea7e3bce75d8adb646d73700f5cdd7f0d38adc46
Maira644 <mairaasghar181@gmail.com>
2026-04-12 15:09:37 +0500

    Update room.py

--- room.py
+++ room.py
@@ -226,7 +226,7 @@ class RoomBooking:
             messagebox.showerror("Error","All fields are required",parent=self.root)
         else:
             try:
-                conn=mysql.connector.connect(host="localhost",username="root",password="1234567890##",database="suite")
+                conn=mysql.connector.connect(host="localhost",username="root",password=config.DB_PASSWORD,database="suite")
                 my_cursor=conn.cursor()
                 my_cursor.execute("INSERT INTO room VALUES(%s,%s,%s,%s,%s,%s,%s)",(
                                                                                     self.var_contact.get(),
